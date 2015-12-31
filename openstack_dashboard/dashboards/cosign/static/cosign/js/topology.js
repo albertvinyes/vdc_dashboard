7ac31556f7c1f8f9dba5bb018652f3ac29d3f31b
Vinyes, Albert <albert.vinyes@i2cat.net>
2015-12-31 14:06:08 +0100

    Drag & Drop implemented in the topology panel. Happy New Year!

---
+++ openstack_dashboard/dashboards/cosign/static/cosign/js/topology.js
@@ -0,0 +1,87 @@
+/* Additional JavaScript for cosign. */
+jsPlumb.ready(function() {
+	/* Init */
+	jsPlumb.importDefaults({ 
+	  ConnectionsDetachable:true
+	});
+	var instance = jsPlumb.getInstance();
+	/* Events */
+	
+
+	/* Make elements draggable within the container bounds */
+	var nonPlumbing = jsPlumb.getInstance();
+	jsPlumb.setContainer($(".topology-container"));
+	
+	jsPlumb.draggable($(".box"), {
+	   containment:true
+	});
+	/* Define the anchors appearance and behaviour*/
+	var exampleDropOptions = {
+        tolerance: "touch",
+        hoverClass: "dropHover",
+        detachable:true,
+        activeClass: "dragActive"
+    };
+	var exampleColor = "#3F51B5";
+	var exampleEndpoint1 = {
+        endpoint: "Rectangle",
+        paintStyle: { width: 25, height: 21, fillStyle: exampleColor },
+        isSource: true,
+        reattach: true,
+        scope: "blue",
+        maxConnections: 30,
+        connectorStyle: {
+            gradient: {stops: [
+                [0, exampleColor],
+                [0.5, "#09098e"],
+                [1, exampleColor]
+            ]},
+            lineWidth: 3,
+            strokeStyle: exampleColor,
+            dashstyle: "8 2"
+        },
+        isTarget: true,
+        beforeDrop: function (params) {
+        	return confirm("Connect " + params.sourceId + " to " + params.targetId + "?");
+        },
+        dropOptions: exampleDropOptions
+    };
+	var exampleEndpoint2 = {
+        endpoint: ["Dot", {radius: 12} ],
+        anchor: "BottomLeft",
+        paintStyle: { fillStyle: exampleColor, opacity: 0.5 },
+        isSource: true,
+        scope: 'yellow',
+        connectorStyle: {
+            strokeStyle: exampleColor,
+            lineWidth: 4
+        },
+        connector: "Bezier",
+		maxConnections: 30,
+        isTarget: true,
+        dropOptions: exampleDropOptions,
+            beforeDetach: function (conn) {
+                return confirm("Detach connection?");
+            },
+            onMaxConnections: function (info) {
+                alert("Cannot drop connection " + info.connection.id + " : maxConnections has been reached on Endpoint " + info.endpoint.id);
+            }
+    };
+
+	jsPlumb.addEndpoint("box-1", { 
+	  anchor:["Bottom", "Continuous"]
+	}, exampleEndpoint1);
+
+	jsPlumb.addEndpoint("box-2", { 
+	  anchor:"Bottom"
+	}, exampleEndpoint1);
+
+	jsPlumb.addEndpoint("box-3", { 
+	  anchor:"Bottom"
+	}, exampleEndpoint1);
+
+	jsPlumb.addEndpoint("box-4", { 
+	  anchor:"Bottom"
+	}, exampleEndpoint1);    
+
+});
